db4af06c9f3178389cb46a8a275779d6a4db95a3
Shivam Deswal <129525543+shivamdeswal@users.noreply.github.com>
2025-06-26 10:47:02 +0530

    nextjs setup

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   router.replace("/signup");
+  // }, [router]);
+  return <div>"Secure User Management App"</div>;
+}
